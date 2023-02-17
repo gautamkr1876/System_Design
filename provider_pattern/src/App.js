cb48f8a7c4f14bf7b844cf94c8cb6d64f488311a
Gautam Kumar <54227183+gautamkr1876@users.noreply.github.com>
2023-02-17 16:46:26 +0000

    initial commit

---
+++ provider_pattern/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  const data = { title: "props dealing", text: "text" };
+  return (
+    <div>
+      <SideBar data={data} />
+      <Content data={data} />
+    </div>
+  )
+}
+
+const SideBar = ({ data }) => <List data={data} />
+const List = ({ data }) => <ListItem data={data} />
+const ListItem = ({ data }) => <span>{data.listItem}</span>
+
+const Content = ({ data }) => (
+  <div>
+    <Header data={data} />
+    <Block data={data} />
+  </div>
+)
+const Header = ({ data }) => <div>{data.title}</div>
+const Block = ({ data }) => <Text data={data} />
+const Text = ({ data }) => <h1>{data.text}</h1>
+export default App;
